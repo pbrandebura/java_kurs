b624e3840bbc167786fd6f53165e0a061ac1c48b
Patryk Brandebura <p.brandebura@gmail.com>
2019-04-07 22:13:26 +0200

    Added test for distance between points

---
+++ sandbox/src/test/java/stqa/ptf/sandbox/PointTest.java
@@ -0,0 +1,31 @@
+package stqa.ptf.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+  @Test
+  public void testPoint(){
+    Point p1 = new Point(5, 5);
+    Point p2 = new Point(5, 0);
+    double expectedResult = 5;
+    Assert.assertEquals(p1.distance(p1, p2), expectedResult);
+  }
+
+  @Test
+  public void testPoint2(){
+    Point p1 = new Point(-3, 4);
+    Point p2 = new Point(5, 4);
+    double expectedResult = 8;
+    Assert.assertEquals(p1.distance(p1, p2),expectedResult);
+  }
+
+  @Test
+  public void testPoint3(){
+    Point p1 = new Point(5, -4);
+    Point p2 = new Point(0, 8);
+    double expectedResult = 13;
+    Assert.assertEquals(p1.distance(p1, p2), expectedResult);
+  }
+}
--- sandbox/src/test/java/stqa/ptf/sandbox/SquareTest.java
+++ sandbox/src/test/java/stqa/ptf/sandbox/SquareTest.java
@@ -11,5 +11,5 @@ public class SquareTest {
     double expectedResults = 25;
     Assert.assertEquals(s.area(), expectedResults);
   }
-  
+
 }
