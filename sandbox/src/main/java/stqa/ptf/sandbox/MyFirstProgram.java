aba88b7e215c0e98f14c4f95ae470cfb8c057a99
Patryk Brandebura <p.brandebura@gmail.com>
2019-04-07 14:55:11 +0200

    Project disabled from GIT repo in IJ

--- sandbox/src/main/java/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/stqa/ptf/sandbox/MyFirstProgram.java
@@ -1,10 +1,10 @@
 package stqa.ptf.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        System.out.println("Hello World");
-    }
+    System.out.println("Hello World");
+  }
 
 
 }
