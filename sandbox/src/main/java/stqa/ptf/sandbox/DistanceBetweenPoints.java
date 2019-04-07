3e76d0f711c3f78e142f8da5289d320f04ed9f2a
Patryk Brandebura <p.brandebura@gmail.com>
2019-04-07 20:03:31 +0200

    Calculating distance between points - task 2

---
+++ sandbox/src/main/java/stqa/ptf/sandbox/DistanceBetweenPoints.java
@@ -0,0 +1,13 @@
+package stqa.ptf.sandbox;
+
+
+public class DistanceBetweenPoints {
+  public static void main(String[] args) {
+    Point p1 = new Point(5, 3);
+    Point p2 = new Point(2, 3);
+
+    System.out.println("Distance between point A:(" + p1.x + ", " + p1.y + ") and point B:(" + p2.x + ", " + p2.y + ") is " + p2.distance(p1, p2));
+
+
+  }
+}
