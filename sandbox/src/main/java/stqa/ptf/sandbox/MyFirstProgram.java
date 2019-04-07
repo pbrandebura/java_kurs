751378ac20ceb562e3cf0b7a62e793003826b265
Patryk Brandebura <p.brandebura@gmail.com>
2019-04-07 18:03:03 +0200

    Few functions added

--- sandbox/src/main/java/stqa/ptf/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/stqa/ptf/sandbox/MyFirstProgram.java
@@ -2,8 +2,29 @@ package stqa.ptf.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
+    hello("Patryk");
 
-    System.out.println("Hello World");
+    double len = 8;
+    System.out.println("Powierzchnia kwadratu o boku " + len + " = " + area(len));
+
+
+    double bok1 = 5;
+    double bok2 = 3.5;
+    System.out.println("Powierzchnia prostokata o bokach " + bok1 + " i " + bok2 + " = " + rectangle(bok1, bok2));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+
+
+  }
+
+  public static double area(double l) {
+    return l * l;
+  }
+
+  public static double rectangle(double a, double b) {
+    return a * b;
   }
 
 
