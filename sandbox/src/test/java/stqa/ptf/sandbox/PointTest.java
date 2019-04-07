e3a14d88ce60f147c301c16695a446b5fc9a7a32
Patryk Brandebura <p.brandebura@gmail.com>
2019-04-07 22:18:14 +0200

    New test in PointTest

--- sandbox/src/test/java/stqa/ptf/sandbox/PointTest.java
+++ sandbox/src/test/java/stqa/ptf/sandbox/PointTest.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class PointTest {
 
   @Test
-  public void testPoint(){
+  public void testPoint() {
     Point p1 = new Point(5, 5);
     Point p2 = new Point(5, 0);
     double expectedResult = 5;
@@ -14,18 +14,26 @@ public class PointTest {
   }
 
   @Test
-  public void testPoint2(){
+  public void testPoint2() {
     Point p1 = new Point(-3, 4);
     Point p2 = new Point(5, 4);
     double expectedResult = 8;
-    Assert.assertEquals(p1.distance(p1, p2),expectedResult);
+    Assert.assertEquals(p1.distance(p1, p2), expectedResult);
   }
 
   @Test
-  public void testPoint3(){
+  public void testPoint3() {
     Point p1 = new Point(5, -4);
     Point p2 = new Point(0, 8);
     double expectedResult = 13;
+    Assert.assertEquals(p1.distance(p1, p2), expectedResult);
+  }
+
+  @Test
+  public void testPoint4() {
+    Point p1 = new Point(-3, 2);
+    Point p2 = new Point(5, 8);
+    double expectedResult = 10;
     Assert.assertEquals(p1.distance(p1, p2), expectedResult);
   }
 }
