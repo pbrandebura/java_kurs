a4da9d6282deff74913e9369524507caca88ba27
Patryk Brandebura <p.brandebura@gmail.com>
2019-05-12 20:14:26 +0200

    Comparing in the creationGroupTest

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,17 +13,21 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", "test2", "test3");
+    GroupData group = new GroupData("test2", "test2", "test3");
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for(GroupData g: after){
-      if(g.getId() > max){
-        max = g.getId();
-      }
-    }
+//    int max = 0;
+//    for(GroupData g: after){
+//      if(g.getId() > max){
+//        max = g.getId();
+//      }
+//    }
+
+    //change list into stream, compare object, selecting max and get ID
+    int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+
     group.setId(max);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
