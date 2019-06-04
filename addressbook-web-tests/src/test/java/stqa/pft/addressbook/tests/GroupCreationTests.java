a71b98d71f892d88acd4b4cc57f4baa6ca0bc026
Patryk Brandebura <p.brandebura@gmail.com>
2019-06-04 21:17:40 +0200

    DataProvider

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,26 +1,46 @@
 package stqa.pft.addressbook.tests;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import stqa.pft.addressbook.model.GroupData;
 import stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/group.csv")));
+    String line = reader.readLine();
+    while(line != null){
+      String[] split = line.split(",");
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("test2");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
+    assertThat(after.size(), equalTo(before.size() + 1));
     assertThat(after, equalTo(
             before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
+
 
   @Test
   public void testBadGroupCreation() throws Exception {
