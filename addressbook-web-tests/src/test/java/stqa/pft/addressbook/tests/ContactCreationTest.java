18bf52cc66446d8359036d9ecda8fc1187e349a2
Patryk Brandebura <p.brandebura@gmail.com>
2019-06-09 16:22:03 +0200

    Json data provider for creatingContact test

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -1,24 +1,47 @@
 package stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import stqa.pft.addressbook.model.ContactData;
 import stqa.pft.addressbook.model.Contacts;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTest extends TestBase {
 
-  @Test(enabled = true)
-  public void testContactCreation() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+    }.getType());
+    return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) throws Exception {
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/images.jpg");
-    ContactData contact = new ContactData().withFirstname("first_name").withMiddlename("middlename").withLastname("lastname")
-            .withNickname("nickname").withHomePhone("111").withMobilePhone("222").withWorkPhone("333").withEmail("lll@lll.ll")
-            .withGroup("test 1").withPhoto(photo);
-    app.contact().create(contact, true);
+    app.contact().create(contact.withPhoto(photo), true);
     app.goTo().homePage();
     assertThat(app.contact().count(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
