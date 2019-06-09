07feac956f941839a0626e8147f0956cb24d9abd
Patryk Brandebura <p.brandebura@gmail.com>
2019-06-09 19:47:25 +0200

    Logback

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/TestBase.java
@@ -1,15 +1,25 @@
 package stqa.pft.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import stqa.pft.addressbook.appmanager.ApplicationManager;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
 
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
+
 
   //replace inheritance with delegation
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws Exception {
@@ -21,4 +31,13 @@ public class TestBase {
     app.stop();
   }
 
+  @BeforeMethod
+  public void logTestStart(Method m, Object[] p) {
+    logger.info("Start test: " + m.getName() + " with parameters " + Arrays.asList(p));
+  }
+
+  @AfterMethod
+  public void logTestStop(Method m) {
+    logger.info("Stop test: " + m.getName());
+  }
 }
